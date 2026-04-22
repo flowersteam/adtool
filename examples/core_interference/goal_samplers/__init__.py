af4bf604d880623801613314e320d9d0db391fc3
Roirtur <arthur.macdonald.dev@gmail.com>
2026-04-22 17:02:36 +0200

    Add QMCMinMaxGoalSampler for goal sampling using Quasi-Monte Carlo

--- examples/core_interference/goal_samplers/__init__.py
+++ examples/core_interference/goal_samplers/__init__.py
@@ -1,3 +1,6 @@
+from examples.core_interference.goal_samplers.qmc_minmax_goal_sampler import (
+    QMCMinMaxGoalSampler,
+)
 from examples.core_interference.goal_samplers.random_minmax_goal_sampler import (
     RandomMinMaxGoalSampler,
 )
