87eec1c171af2b1fdd5a102820040ec68cb7b5bd
Roirtur <arthur.macdonald.dev@gmail.com>
2026-06-25 10:56:00 +0200

    feat: add MutualMissHeatmapModule and corresponding analysis configuration

---
+++ examples/embedded_systems/examples/core_interferences/analysis_modules/__init__.py
@@ -0,0 +1,5 @@
+from adtool.examples.embedded_systems.examples.core_interferences.analysis_modules.mutual_miss_heatmap import (
+    MutualMissHeatmapModule,
+)
+
+__all__ = ["MutualMissHeatmapModule"]
